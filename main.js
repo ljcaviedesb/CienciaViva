ba237b9aebe38d4df26c410fafe1ce6b30b44b2e
juliana caviedes <ljcaviedesb@unal.edu.co>
2025-05-27 23:23:14 -0500

    Add js functios for display info and menu

---
+++ main.js
@@ -0,0 +1,25 @@
+function toggleInfo(id) {
+    const card = document.getElementById(id);
+    const button = card.querySelector('.btn-toggle');
+    card.classList.toggle('expanded');
+    button.textContent = card.classList.contains('expanded') ? 'Menos info ↑' : 'Más info →';
+    button.textContent = card.classList.contains('expanded') ? 'Ir al formulario' : 'Más info →';
+}
+
+function toggleMenu() {
+    const nav = document.getElementById('navMenu');
+    nav.classList.toggle('show');
+}
+
+function toggleInfo(id) {
+    const card = document.getElementById(id);
+    const button = card.querySelector('.btn-toggle');
+    
+    card.classList.toggle('expanded');
+    
+    if (card.classList.contains('expanded')) {
+      button.textContent = 'Menos info ↑';
+    } else {
+      button.textContent = 'Más info →';
+    }
+}
